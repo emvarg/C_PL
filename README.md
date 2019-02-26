d9e98738274e940f7bfebe88f5374607408971dc
Emmanuel Vargas Blanco <emvargcr@gmail.com>
2019-02-25 19:46:08 -0600

    Ejercicio 1: Cambiar valores formato para variables
    Se deben revisar las líneas que imprimen los tipos de datos
    y corregir los valores de acuerdo al tipo.
    
    Para resolver este ejecicio, se puede consultar el enlace
    en el README.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 Curso de C en el PRIS Lab.
+
+Documentación para ejercicio 1
+
+* https://www.geeksforgeeks.org/data-types-in-c/
+
--- src/tipos_de_datos.c
+++ src/tipos_de_datos.c
@@ -3,12 +3,22 @@
 int main(){
 
   int variable;
+  char dato_caracter = 'C';
+  unsigned int dato_uint = 2345234;
+  float dato_float = 123.34;
+  double dato_double = 123123123.23234;
+  long double dato_long_double = 323423.43434;
 
   variable = 10;
 
   printf("-------- Labo de C -------- \n");
 
   printf("El contenido de la variable es: %d, está formada por %ld bytes \n", variable, sizeof(variable));
+  printf("El contenido de la variable es: %d, está formada por %ld bytes \n", dato_caracter, sizeof(dato_caracter));
+  printf("El contenido de la variable es: %d, está formada por %ld bytes \n", dato_uint, sizeof(dato_uint));
+  printf("El contenido de la variable es: %d, está formada por %ld bytes \n", dato_float, sizeof(dato_float));
+  printf("El contenido de la variable es: %d, está formada por %ld bytes \n", dato_double, sizeof(dato_double));
+  printf("El contenido de la variable es: %d, está formada por %ld bytes \n", dato_long_double, sizeof(dato_long_long_double));
 
   return variable;
 }
