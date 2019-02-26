5c1a4a173d74d7f552824f9e08baec60a4d32170
Emmanuel Vargas Blanco <emvargcr@gmail.com>
2019-02-25 21:12:39 -0600

    Ejemplo de revision de parametros

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ Documentación para ejercicio 1
 
 * https://www.geeksforgeeks.org/data-types-in-c/
 
+Como obtener las funciones en un archivo de código fuente
+
+  grep -n -E '^\w.*\(' stdlib.h | grep atoi
+
+La línea anterior, devuelve las ocurrencias de atoi
--- src/entrada_salida_de_datos.c
+++ src/entrada_salida_de_datos.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]){
 
@@ -7,9 +8,27 @@ int main(int argc, char *argv[]){
 
   printf("El nombre del progrma es %s \n", argv[0]);
 
-  for (i = 0; i < argc; i++){
+  for (i = 1; i < argc; i++){
     printf("Parámetro de entrada %d, es: %s \n",i, argv[i]);
   }
+
+  // Impresion de líneas controlada por parámetros de entrada
+
+  // Revision que evita que el usuario corra el programa sin
+  // parámetros
+  if (argv[1] == NULL){
+    contador = 2;
+  } else {
+    contador = atoi(argv[1]);
+  }
+
+  for(i=0; i < contador; i++){
+    printf("***********************\n");
+    printf("Valor del contador :%d \n", i);
+    printf("***********************\n");
+  }
+
+    printf("Valor del contador :%d \n", i);
 
   return 0;
 }
