ba299e52bc9f8596efacc8a709fc37db562ab376
Emmanuel Vargas Blanco <emvargcr@gmail.com>
2019-02-28 19:36:04 -0600

    Agrego ejemplo de arrays de dos dimensiones

--- src/arrays.c
+++ src/arrays.c
@@ -2,22 +2,41 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define I_DATOS_MAX 10 
+#define J_DATOS_MAX 2
+#define RANDOM_MAX 100
 
 int main(int argc, char *argv[])
 {
-  int my_data_s0[10];
-  int i;
+  int my_data_s0[I_DATOS_MAX];
+  int my_data_s1[I_DATOS_MAX][J_DATOS_MAX];
+  int i, j;
   srandom(time(NULL));
 
-  // Fill data
-  for (i = 0; i < 10; i++){
-    my_data_s0[i] = random()%10;
+  // Fill 1D data
+  for (i = 0; i < I_DATOS_MAX; i++){
+    my_data_s0[i] = random()%RANDOM_MAX;
   }
 
-  // Print data
-  for (i = 0; i < 10; i++){
+  // Print 1D data
+  for (i = 0; i < I_DATOS_MAX; i++){
     printf("data_s0[%d] : %d\n", i, my_data_s0[i]);
   }
+
+  // Fill 2D data
+  for (i = 0; i < I_DATOS_MAX; i++){
+    for (j = 0; j < J_DATOS_MAX; j++){
+    my_data_s1[i][j] = random()%RANDOM_MAX;
+    }
+  }
+
+  // Print 2D data
+  for (i = 0; i < I_DATOS_MAX; i++){
+    for (j = 0; j < J_DATOS_MAX; j++){
+    printf("data_s0[%d][%d] : %d\n", i, j, my_data_s1[i][j]);
+    }
+  }
+
 
   return 0;
 }
